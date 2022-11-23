bace4f92df53cabdfd8cb85b9f4e95ed17644e83
AWalker2025 <112598458+AWalker2025@users.noreply.github.com>
2022-11-23 13:16:48 -0500

    Visual

--- js/page.js
+++ js/page.js
@@ -1,2 +1,53 @@
 //Plays the game
-function game
+function game(x){
+    sessionStorage.playerchoice=x
+
+    var x=math.cell (math.random()*3)
+    
+    if (y == 1) {
+        sessionStorage.opponentChoice = "rock";
+    }
+    else if (y == 2) {
+        sessionStorage.opponentChoice = "paper";
+    }
+    else if (y == 3) {
+        sessionStorage.opponentChoice = "scissors";
+    }
+
+    if ((sessionStorage.mychoice == "rock") && (sessionStorage.opponentChoice == "paper")) {
+        sessionStorage.result = "lose"
+    }
+    else if ((sessionStorage.mychoice == "rock") && (sessionStorage.opponentChoice == "scissors")) {
+        sessionStorage.result = "win"
+    }
+    else if ((sessionStorage.mychoice == "paper") && (sessionStorage.opponentChoice == "scissors")) {
+        sessionStorage.result = "lose"
+    }
+    else if ((sessionStorage.mychoice == "paper") && (sessionStorage.opponentChoice == "rock")) {
+        sessionStorage.result = "win"
+    }
+    else if ((sessionStorage.mychoice == "scissors") && (sessionStorage.opponentChoice == "rock")) {
+        sessionStorage.result = "lose"
+    }
+    else if ((sessionStorage.mychoice == "scissors") && (sessionStorage.opponentChoice == "paper")) {
+        sessionStorage.result = "win"
+    }
+    else if (sessionStorage.mychoice == sessionStorage.opponentChoice) {
+        sessionStorage.result = "tie"
+    }
+    
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
