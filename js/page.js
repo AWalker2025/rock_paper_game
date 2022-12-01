79960304def82c744d9d75d93f67f44c15644988
AWalker2025 <112598458+AWalker2025@users.noreply.github.com>
2022-11-30 21:08:12 -0500

    Scoreboard

--- js/page.js
+++ js/page.js
@@ -1,8 +1,12 @@
-//Plays the game
-function game(x){
-    sessionStorage.playerchoice=x
+sessionStorage.wins = 0;
+sessionStorage.losses = 0;
 
-    var x=math.cell (math.random()*3)
+
+//Plays the game
+function game (x) {
+    sessionStorage.mychoice = x;
+
+    var y = Math.ceil(Math.random() *3);
     
     if (y == 1) {
         sessionStorage.opponentChoice = "rock";
@@ -15,44 +19,47 @@ function game(x){
     }
 
     if ((sessionStorage.mychoice == "rock") && (sessionStorage.opponentChoice == "paper")) {
-        sessionStorage.result = "lose"
+        sessionStorage.result = "lose";
     }
     else if ((sessionStorage.mychoice == "rock") && (sessionStorage.opponentChoice == "scissors")) {
-        sessionStorage.result = "win"
+        sessionStorage.result = "win";
     }
     else if ((sessionStorage.mychoice == "paper") && (sessionStorage.opponentChoice == "scissors")) {
-        sessionStorage.result = "lose"
+        sessionStorage.result = "lose";
     }
     else if ((sessionStorage.mychoice == "paper") && (sessionStorage.opponentChoice == "rock")) {
-        sessionStorage.result = "win"
+        sessionStorage.result = "win";
     }
     else if ((sessionStorage.mychoice == "scissors") && (sessionStorage.opponentChoice == "rock")) {
-        sessionStorage.result = "lose"
+        sessionStorage.result = "lose";
     }
     else if ((sessionStorage.mychoice == "scissors") && (sessionStorage.opponentChoice == "paper")) {
-        sessionStorage.result = "win"
+        sessionStorage.result = "win";
     }
     else if (sessionStorage.mychoice == sessionStorage.opponentChoice) {
-        sessionStorage.result = "tie"
+        sessionStorage.result = "tie";
     }
+    
+
     //Creates Notification
     if (sessionStorage.result == "tie") {
         document.getElementById ("result").innerHTML = "It's A Tie!";
+        
     }
     else if (sessionStorage.result == "win") {
         document.getElementById ("result").innerHTML = "You Won!";
+        sessionStorage.wins++;
+        document.getElementById ("pscore").innerHTML = sessionStorage.wins;
     }
     else if (sessionStorage.result == "lose") {
         document.getElementById ("result").innerHTML = "You Lose!";
+        sessionStorage.losses++;
+        document.getElementById ("oscore").innerHTML = sessionStorage.losses;
     }
 //Shows Opponets Img
-document.getElementById
+document.getElementById("o-img").src ='svg/' + sessionStorage.opponentChoice + '.svg';
 
-
-
-
-
-    }
+}
 
 
 
